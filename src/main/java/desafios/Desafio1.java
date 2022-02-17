b36a481c518df81a6446691a4f6b1c3ec64ee3a9
Laisa Cunha <lai_cunha@outlook.com>
2022-02-17 13:47:29 -0200

    adicionando logica desafio3 anagramas

--- src/main/java/desafios/Desafio1.java
+++ src/main/java/desafios/Desafio1.java
@@ -14,11 +14,12 @@ public class Desafio1 {
 
     /**
      * Metodo que mostra na tela uma escada de tamanho n utilizando o caractere * e espaços.
-     * @param n
+     * de acordo com a quantidade adicionada no parametro.
+     * @param qtd
      */
-    static void escada(int n) {
-        for (int linha=1; linha<=n; linha++){
-            for(int espaco = 0;espaco<n-linha;espaco++) {
+    static void escada(int qtd) {
+        for (int linha=1; linha<=qtd; linha++){
+            for(int espaco = 0;espaco<qtd-linha;espaco++) {
                 System.out.print(" ");
             }
             for (int col=linha;col >=1;col--){
--- src/main/java/desafios/Desafio3.java
+++ src/main/java/desafios/Desafio3.java
@@ -1,4 +1,43 @@
 package desafios;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Desafio3 {
+    public static void main(String[] args) {
+        subString("laisa");
+    }
+
+
+    /**
+     * Usando frequências de caracteres como chaves e contagens correspondentes como valores.
+     * Resouvendo esse problema iterando em todas as
+     * substrings e contando as frequências de caracteres em cada substring.
+     *
+     * @param s
+     */
+    static void subString(String s){
+        HashMap<String, Integer> map= new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                char[] valC = s.substring(i, j+1).toCharArray();
+                Arrays.sort(valC);
+                String val = new String(valC);
+                if (map.containsKey(val))
+                    map.put(val, map.get(val)+1);
+                else
+                    map.put(val, 1);
+            }
+        }
+        /**
+         * Pares de anagramas que podem ser formados
+         */
+        int anagramPairCount = 0;
+        for(String key: map.keySet()){
+            int n = map.get(key);
+            anagramPairCount += (n * (n-1))/2;
+        }
+        System.out.println(anagramPairCount);
+    }
 }
+
